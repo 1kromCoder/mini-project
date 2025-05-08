95684114a250d12558d446a2e7887d8bde44bac9
Azamat <azamatomonkeldiyev@gmail.com>
2025-05-08 17:50:00 +0500

    add bearer

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,8 @@ async function bootstrap() {
     .setTitle('Restaurant Management')
     .setDescription('The Restaurant Management API description')
     .setVersion('1.0')
+    .addSecurityRequirements('bearer', ['bearer'])
+    .addBearerAuth()
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, documentFactory);
